2c90a65efd4aeb7b9b95a82866489ea863b6a6fe
Hosseinghafouri <hossenghafouri@gmail.com>
2022-09-23 22:46:51 +0330

    Start

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const $ = document;
+let autoCompleteWrapper = $.querySelector('.search-box');
+let searchInputElem = $.querySelector('.search-input');
+let autoCompleteBox = $.querySelector('.autocom-box');
+
+searchInputElem.addEventListener('keyup', () => {
+    let searchValue = searchInputElem.value;
+
+    if (searchValue) {
+        autoCompleteWrapper.classList.add("active");
+
+        let filteredWords = suggestions.filter(word => word.toLowerCase().startsWith(searchValue.toLowerCase()));
+        suggestionsWordGenerator(filteredWords);
+
+    }else {
+        autoCompleteWrapper.classList.remove("active");
+    };
+   
+});
+
+function suggestionsWordGenerator(wordsArray) {
+    let listItemsArray = wordsArray.map((word)=>{
+        return '<li>' + word + '</li>';
+    });
+
+    let customListItem;
+    if (!listItemsArray.length) {
+        customListItem = '<li>' + searchInputElem.value + '</li>';
+    } else {
+        customListItem = listItemsArray.join('');
+    }
+
+    autoCompleteBox.innerHTML = customListItem;
+    selectList();
+};
+
+function selectList() {
+    let allListItems = autoCompleteBox.querySelectorAll("li");
+    allListItems.forEach((wordItem)=>{
+        wordItem.addEventListener('click', (event)=>{
+            searchInputElem.value = event.target.textContent;
+            autoCompleteWrapper.classList.remove("active");
+        });
+    });
+};
